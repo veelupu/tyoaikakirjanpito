8afc9d81be4dba6b6bb62d541fca3fb6f9f543b8
Veera Lupunen <veera.lupunen@helsinki.fi>
2021-05-28 19:44:33 +0300

    Add login

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
+# Copyright (c) 2021 Veera Lupunen
+
 from flask import Flask
+from flask import redirect, render_template, request, session
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return "Työaikakirjanpito"
+    options = ["merkitä ylös työntuntisi", "pysyä kärryillä tehtyjen tuntien määrästä", "kannustaa itseäsi toisaalta töiden tekoon ja toisaalta ansaittuun lepoon."]
+    return render_template("index.html", items=options)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # PUUTTUU: SALASANAN JA TUNNUKSEN TARKISTAMINEN
+    session["username"] = username
+    return redirect("/home")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+    
+@app.route("/home")
+def home():
+    return render_template("home.html")
