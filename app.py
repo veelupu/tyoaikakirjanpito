ccf7644e263f1e02119c049f5ea24187d7cc5ed9
Veera Lupunen <veera.lupunen@helsinki.fi>
2021-05-30 22:21:46 +0300

    Fix database URL

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///veeralupunen"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
